b75749168ef7965bd92cc5930fde9e87689b61db
Eli Sachs <easachs13@gmail.com>
2022-08-29 17:58:05 -0600

    Create item/merchant serializers

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 
-      resources :merchants, only: [:index, :show]# do
-      #   resources :items, only: [:index]
-      # end
+      resources :merchants, only: [:index, :show] do
+        resources :items, only: [:index], controller: :merchant_items
+      end
 
-      resources :items# do
-      #   resources :merchants, only: [:index]
-      # end
+      resources :items do
+        resources :merchants, only: [:index], controller: :item_merchants
+      end
 
     end
   end
