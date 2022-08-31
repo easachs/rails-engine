b45230d2da4e201336e8b8b9424b844309158239
Eli Sachs <easachs13@gmail.com>
2022-08-31 02:12:13 -0600

    Improve find controller for Postman

--- app/controllers/api/v1/find_controller.rb
+++ app/controllers/api/v1/find_controller.rb
@@ -4,42 +4,84 @@ module Api
   module V1
     class FindController < ApplicationController
       def find_merchant
-        render json: MerchantSerializer.new(Merchant.find_name(search_params[:name]))
+        if Merchant.find_name(search_params[:name]).nil? || search_params[:name].nil? || search_params[:name] == ''
+          empty400
+        else
+          render json: MerchantSerializer.new(Merchant.find_name(search_params[:name]))
+        end
       end
 
       def find_all_merchants
-        render json: MerchantSerializer.new(Merchant.find_all_name(search_params[:name]))
+        if Merchant.find_all_name(search_params[:name]).nil? || search_params[:name].nil? || search_params[:name] == ''
+          empty400
+        else
+          render json: MerchantSerializer.new(Merchant.find_all_name(search_params[:name]))
+        end
       end
 
       def find_item
-        if params[:name] && (params[:min_price] || params[:max_price])
-          render status: 400
-        elsif params[:name]
-          render json: ItemSerializer.new(Item.find_name(params[:name]))
-        elsif params[:min_price] && params[:max_price]
-          render json: ItemSerializer.new(Item.price_range(params[:min_price], params[:max_price]))
-        elsif params[:min_price]
-          render json: ItemSerializer.new(Item.min_price(params[:min_price]))
-        elsif params[:max_price]
-          render json: ItemSerializer.new(Item.max_price(params[:max_price]))
+        if Item.find_name(search_params[:name]).nil? || search_params[:name] == ''
+          empty400
+        elsif search_params[:name] && (search_params[:min_price] || search_params[:max_price])
+          empty400
+        elsif search_params[:name]
+          render json: ItemSerializer.new(Item.find_name(search_params[:name]))
+        elsif search_params[:min_price] && search_params[:max_price]
+          if Item.price_range(search_params[:min_price],
+                              search_params[:max_price]).nil? || search_params[:min_price] == '' || search_params[:max_price] == ''
+            empty400
+          else
+            render json: ItemSerializer.new(Item.price_range(search_params[:min_price], search_params[:max_price]))
+          end
+        elsif search_params[:min_price]
+          if Item.min_price(search_params[:min_price]).nil? || search_params[:min_price].to_f.negative? || search_params[:min_price] == ''
+            empty400
+          else
+            render json: ItemSerializer.new(Item.min_price(search_params[:min_price]))
+          end
+        elsif search_params[:max_price]
+          if search_params[:max_price].to_f.negative? || search_params[:max_price] == ''
+            empty400
+          else
+            render json: ItemSerializer.new(Item.max_price(search_params[:max_price]))
+          end
         else
-          render status: 400
+          empty400
         end
       end
 
       def find_all_items
-        if params[:name] && (params[:min_price] || params[:max_price])
-          render status: 400
-        elsif params[:name]
-          render json: ItemSerializer.new(Item.find_all_name(params[:name]))
-        elsif params[:min_price] && params[:max_price]
-          render json: ItemSerializer.new(Item.price_range_all(params[:min_price], params[:max_price]))
-        elsif params[:min_price]
-          render json: ItemSerializer.new(Item.min_price_all(params[:min_price]))
-        elsif params[:max_price]
-          render json: ItemSerializer.new(Item.max_price_all(params[:max_price]))
+        if Item.find_all_name(search_params[:name]).nil? || search_params[:name] == ''
+          empty400
+        elsif search_params[:name] && (search_params[:min_price] || search_params[:max_price])
+          empty400
+        elsif search_params[:name]
+          render json: ItemSerializer.new(Item.find_all_name(search_params[:name]))
+
+        elsif search_params[:min_price] && search_params[:max_price]
+          if Item.price_range_all(search_params[:min_price],
+                                  search_params[:max_price]).nil? || search_params[:min_price] == '' || search_params[:max_price] == ''
+            empty400
+          else
+            render json: ItemSerializer.new(Item.price_range_all(search_params[:min_price], search_params[:max_price]))
+          end
+
+        elsif search_params[:min_price]
+          if Item.min_price_all(search_params[:min_price]).nil? || search_params[:min_price].to_f.negative? || search_params[:min_price] == ''
+            empty400
+          else
+            render json: ItemSerializer.new(Item.min_price_all(search_params[:min_price]))
+          end
+
+        elsif search_params[:max_price]
+          if search_params[:max_price].to_f.negative? || search_params[:max_price] == ''
+            empty400
+          else
+            render json: ItemSerializer.new(Item.max_price_all(search_params[:max_price]))
+          end
+          
         else
-          render status: 400
+          empty400
         end
       end
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  rescue_from ActiveRecord::RecordNotFound, with: :not_found_404
-  rescue_from ActiveRecord::RecordInvalid, with: :not_found_404
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found404
+  rescue_from ActiveRecord::RecordInvalid, with: :not_found404
 
-  def not_found_404
-    render status: 404
+  def not_found404
+    render json: { error: 'Not found' }, status: 404
+  end
+
+  def empty400
+    render json: { error: 'No results', data: {} }, status: 400
   end
 end
